0792fdda875afe3d27ab9401980db7941ef0ffbe
RobinSrk <robinsrk3@gmail.com>
2022-12-10 12:37:50 +0600

    updated designs

--- app/src/main/java/com/example/buspayment/screens/ScanScreen.kt
+++ app/src/main/java/com/example/buspayment/screens/ScanScreen.kt
@@ -13,6 +13,8 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,8 +25,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
@@ -97,7 +101,13 @@ fun ScanScreen(navController: NavController) {
 					}
 					previewView
 				},
-				modifier = Modifier.weight(1f)
+				modifier = Modifier
+					.weight(1f)
+					.padding(
+						40.dp,
+						300.dp
+					)
+					.clip(RoundedCornerShape(40.dp))
 			)
 			if (code == "Home") {
 				Button(onClick = { navController.navigate(Screens.Home.route) }) {
